24c167cab4de481c1b0f8a8d415e34ee4b27a800
Bastien Robert <bastienrbt@protonmail.com>
2017-06-11 01:51:13 +0200

    Add Issues as root

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :archives
   resources :issues
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "issues#index"
 end
