33077ccee64e4e56413adb9973507ff2971c7c40
Bastien Robert <bastienrbt@protonmail.com>
2017-06-11 01:20:41 +0200

    Scaffold - Generate archives

---
+++ app/views/archives/_archive.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! archive, :id, :issue_id, :conf_date, :conf_location, :facebookEvent, :time, :video, :audio, :speaker, :created_at, :updated_at
+json.url archive_url(archive, format: :json)
---
+++ db/migrate/20170610232027_create_archives.rb
@@ -0,0 +1,16 @@
+class CreateArchives < ActiveRecord::Migration[5.0]
+  def change
+    create_table :archives do |t|
+      t.references :issue, foreign_key: true
+      t.datetime :conf_date
+      t.string :conf_location
+      t.string :facebookEvent
+      t.integer :time
+      t.string :video
+      t.string :audio
+      t.string :speaker
+
+      t.timestamps
+    end
+  end
+end
