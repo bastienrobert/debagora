bebb6dee10b6eddb4f29e152ed95afde1ea08042
Bastien Robert <bastienrbt@protonmail.com>
2017-06-11 16:27:50 +0200

    Generate static pages and re-organize routes file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  # Devise parameters configuration
   before_action :configure_devise_parameters, if: :devise_controller?
   def configure_devise_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :avatar, :firstname, :lastname, :region, :country, :bio, :password, :password_confirmation, :current_password) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :avatar, :firstname, :lastname, :region, :country, :bio, :password, :password_confirmation, :current_password) }
+  end
+
+  # Static pages
+  def home
+
+  end
+  def about
+
+  end
+  def live
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,24 @@
 Rails.application.routes.draw do
+
+  # ROOT
+  root "application#home"
+
+  # Include ressources
   resources :comments
   resources :archives
   resources :issues
+
+  # Issues special routes
   post '/issues/:id/like', to: 'issues#like'
   post '/issues/:id/unlike', to: 'issues#unlike'
 
+  # Devise configuration : for devise & omniauth
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
 
-  root "issues#index"
+  # Static pages
+  get "live", to: "application#live", as: :live
+  get "about", to: "application#about", as: :about
+
 end
