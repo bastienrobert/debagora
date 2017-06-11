2112baf519b3f43d90002e511abbab48c484d48b
Bastien Robert <bastienrbt@protonmail.com>
2017-06-11 02:44:03 +0200

    Add & configure Paperlcip to make users avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,13 @@ class User < ApplicationRecord
     end
   end
 
+
+  ## Paperclip avatar
+  has_attached_file :avatar, styles: {
+    big: "1200x1200>",
+    medium: "300x300>",
+    thumb: "100x100>"
+  }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610232428) do
+ActiveRecord::Schema.define(version: 20170611002320) do
 
   create_table "archives", force: :cascade do |t|
     t.integer  "issue_id"
@@ -65,6 +65,10 @@ ActiveRecord::Schema.define(version: 20170610232428) do
     t.string   "facebook_id"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
