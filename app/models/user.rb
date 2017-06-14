f568aaf691231986942a1787eaa7de1ec063b694
Bastien Robert <bastienrbt@protonmail.com>
2017-06-14 14:55:13 +0200

    Default profil image (simple grey bg) update

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < ApplicationRecord
     big: "1200x1200>",
     medium: "300x300>",
     thumb: "100x100>"
-  }, default_url: "/images/:style/missing.png"
+  }, default_url: "missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 end
